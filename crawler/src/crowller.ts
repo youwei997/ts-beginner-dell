8a52d2a8cdea2b90dc30f0a7a03ac16817975472
youwei <1690312648@qq.com>
2022-05-18 23:48:41 +0800

    爬取慕课网实战课列表的标题和金额

--- crawler/src/crowller.ts
+++ crawler/src/crowller.ts
@@ -1,10 +1,39 @@
 import superagent from "superagent";
+import cheerio from "cheerio";
+
+interface Course {
+  title: string;
+  price: number;
+}
 
 class Crowller {
-  private rawHtml = "";
+  //解析html
+  getCourseInfo(html: string) {
+    const $ = cheerio.load(html);
+    const courseCard = $(".course-card");
+    const courseInfos: Course[] = [];
+    courseCard.map((index, element) => {
+      // 获取课程的标题
+      const title = $(element).find(".ellipsis2").text();
+      // 获取课程的金额
+      const price = parseInt(
+        $(element).find(".price").text().replace("￥", "")
+      ); //获取金额，并且去除￥
+      courseInfos.push({
+        title,
+        price,
+      });
+    });
+    const result = {
+      time: new Date().getTime(),
+      data: courseInfos,
+    };
+    console.log(result);
+  }
+  // 获取html
   getRawHtml() {
     superagent.get("https://coding.imooc.com/").then((res) => {
-      this.rawHtml = res.text;
+      this.getCourseInfo(res.text);
     });
   }
   constructor() {
