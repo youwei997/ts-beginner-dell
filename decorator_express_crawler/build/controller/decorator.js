51fcdb06801a0b587960b5b171199ff53b424bd0
youwei <1690312648@qq.com>
2022-08-04 20:45:34 +0800

    7-3 多种请求方法装饰器的生成

--- decorator_express_crawler/build/controller/decorator.js
+++ decorator_express_crawler/build/controller/decorator.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.post = exports.get = exports.controller = void 0;
+exports.del = exports.put = exports.post = exports.get = exports.controller = void 0;
 var express_1 = require("express");
 var router = (0, express_1.Router)();
 function controller(target) {
@@ -8,23 +8,43 @@ function controller(target) {
         // 这个key就是class 里的方法名
         var path = Reflect.getMetadata("path", target.prototype, key);
         // 用方法名取到target.prototype 里对应的函数，相当于取class里的函数
+        // 获取请求方法
+        var method = Reflect.getMetadata("method", target.prototype, key);
         var func = target.prototype[key];
-        if (path) {
-            router.get(path, func);
+        if (path && method && func) {
+            console.log(method);
+            router[method](path, func);
         }
     }
 }
 exports.controller = controller;
-function get(path) {
-    return function (target, key) {
-        Reflect.defineMetadata("path", path, target, key);
+// 用于接收各个不同请求方式的函数
+function getRequestDecorator(type) {
+    // 装饰器工厂，用于接收请求路径
+    return function (path) {
+        // 装饰器
+        return function (target, key) {
+            // 将路径存到元数据
+            Reflect.defineMetadata("path", path, target, key);
+            // 将请求方式存到元数据
+            Reflect.defineMetadata("method", type, target, key);
+        };
     };
 }
-exports.get = get;
-function post(path) {
-    return function (target, key) {
-        Reflect.defineMetadata("path", path, target, key);
-    };
-}
-exports.post = post;
+exports.get = getRequestDecorator("get");
+exports.post = getRequestDecorator("post");
+exports.put = getRequestDecorator("put");
+exports.del = getRequestDecorator("delete");
+// export function get(path: string) {
+//   return function (target: any, key: string) {
+//     Reflect.defineMetadata("path", path, target, key);
+//     Reflect.defineMetadata("method", "get", target, key);
+//   };
+// }
+// export function post(path: string) {
+//   return function (target: any, key: string) {
+//     Reflect.defineMetadata("path", path, target, key);
+//     Reflect.defineMetadata("method", "post", target, key);
+//   };
+// }
 exports.default = router;
--- decorator_express_crawler/src/controller/decorator.ts
+++ decorator_express_crawler/src/controller/decorator.ts
@@ -1,28 +1,63 @@
 import { Router } from "express";
 const router = Router();
 
+// enum Method {
+//     get = 'get',
+//     post = 'post',
+// }
+type MethodType = "get" | "post";
+
 export function controller(target: any) {
   for (let key in target.prototype) {
     // 这个key就是class 里的方法名
     const path = Reflect.getMetadata("path", target.prototype, key);
     // 用方法名取到target.prototype 里对应的函数，相当于取class里的函数
+    // 获取请求方法
+    const method: MethodType = Reflect.getMetadata(
+      "method",
+      target.prototype,
+      key
+    );
     const func = target.prototype[key];
-    if (path) {
-      router.get(path, func);
+    if (path && method && func) {
+      console.log(method);
+
+      router[method](path, func);
     }
   }
 }
 
-export function get(path: string) {
-  return function (target: any, key: string) {
-    Reflect.defineMetadata("path", path, target, key);
+// 用于接收各个不同请求方式的函数
+function getRequestDecorator(type: string) {
+  // 装饰器工厂，用于接收请求路径
+  return function (path: string) {
+    // 装饰器
+    return function (target: any, key: string) {
+      // 将路径存到元数据
+      Reflect.defineMetadata("path", path, target, key);
+      // 将请求方式存到元数据
+      Reflect.defineMetadata("method", type, target, key);
+    };
   };
 }
 
-export function post(path: string) {
-  return function (target: any, key: string) {
-    Reflect.defineMetadata("path", path, target, key);
-  };
-}
+export const get = getRequestDecorator("get");
+export const post = getRequestDecorator("post");
+export const put = getRequestDecorator("put");
+export const del = getRequestDecorator("delete");
+
+// export function get(path: string) {
+//   return function (target: any, key: string) {
+//     Reflect.defineMetadata("path", path, target, key);
+//     Reflect.defineMetadata("method", "get", target, key);
+//   };
+// }
+
+// export function post(path: string) {
+//   return function (target: any, key: string) {
+//     Reflect.defineMetadata("path", path, target, key);
+//     Reflect.defineMetadata("method", "post", target, key);
+//   };
+// }
 
 export default router;
