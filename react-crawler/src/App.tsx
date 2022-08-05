5f93ec8c15a322cd49d1476d37bb5743d1e129ae
youwei <1690312648@qq.com>
2022-08-05 17:07:19 +0800

    8-2 编写登陆表单

--- react-crawler/src/App.tsx
+++ react-crawler/src/App.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import { Form, Input, Button } from "antd";
+import { LockOutlined } from "@ant-design/icons";
+import React, { Component } from "react";
+import "./App.css";
+import "./login.css";
 
-const App:React.FC = ()=> {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+// 通过泛型，告诉组件外部传进来的参数有哪些
+interface Props {
+  form: any;
 }
 
+class NormalLoginForm extends Component<Props> {
+  handleSubmit = (e: any) => {
+    e.preventDefault();
+    this.props.form.validateFields((err: any, values: any) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className="login-page">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            {getFieldDecorator("username", {
+              rules: [{ required: true, message: "请输入登录密码!" }],
+            })(
+              <Input
+                prefix={<LockOutlined className="password" />}
+                type="password"
+                placeholder="Password"
+              />
+            )}
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    );
+  }
+}
+
+const App = Form.create({ name: "normal_login" })(NormalLoginForm);
 export default App;
